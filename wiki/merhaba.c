d093bd8ba78912e21018c7d6583e3762316a0a91
bayramkarahan <bayramk@gmail.com>
2023-12-24 21:28:58 +0300

    migration

--- wiki/merhaba.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<readline/readline.h>
-//gcc -o merhaba merhaba.c $(pkgconf --cflags --libs readline) -lncurses
-void main()
-{
-char* msg=readline("Adını Yaz:");
-puts(msg);
-puts("hehe");
-}
